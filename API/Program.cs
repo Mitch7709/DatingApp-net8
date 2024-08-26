27f61d8ec9f30a0fdb022fbc1cd6e7e426c19477
Mitchell Lapora <mlapora1@gmail.com>
2024-08-25 21:04:06 -0500

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,9 @@ builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite(builder.Configur
 
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,6 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 //app.UseHttpsRedirection();
 
 //app.UseAuthorization();
